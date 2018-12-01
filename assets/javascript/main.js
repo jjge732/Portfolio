24d3dfa89094325ded1a4a604118865985510178
John Eastwood <jjge732@gmail.com>
2018-12-01 15:46:32 -0600

    took out about link

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -6,6 +6,8 @@ $('#landingImageContainer header').hover(function() {
     $('#landingImageContainer').append($('<img alt="black & white" id="landingImage">'));
 });
 
+
+
 $(document).scroll('header', function() {
         $('header').attr('id', 'scrolled')
     if ($(window).scrollTop() === 0) {
@@ -17,7 +19,6 @@ setInterval(function() {
     if (window.matchMedia('(max-width: 835px)').matches) {
         $('header h2').remove();
     } else {
-        $('header').html("<h1>John Eastwood</h1><div><h2><a href='#'>About</a></h2><h2><a href='#'>Porfolio</a></h2><h2><a href='#'>Contact</a></h2></div>")
+        $('header').html("<h1>John Eastwood</h1><div><h2><a href='#'>Porfolio</a></h2></div>")
     }
 }, 1);
-
