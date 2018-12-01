ade5308ffac429258bed3a3069198fd7395af008
John Eastwood <jjge732@gmail.com>
2018-12-01 15:24:05 -0600

    added column gap, changed media queries

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -14,7 +14,7 @@ $(document).scroll('header', function() {
 });
 
 setInterval(function() {
-    if (window.matchMedia('(max-width: 799px)').matches) {
+    if (window.matchMedia('(max-width: 835px)').matches) {
         $('header h2').remove();
     } else {
         $('header').html("<h1>John Eastwood</h1><div><h2><a href='#'>About</a></h2><h2><a href='#'>Porfolio</a></h2><h2><a href='#'>Contact</a></h2></div>")
